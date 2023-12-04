1ca4e0d65dd6158662c6e26c17a8f5fe8cab55ed
David Darj <david.darj@saabgroup.com>
2023-12-04 12:37:08 +0100

    Day 01 part 1

---
+++ Day01/Day01.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace AoC
+{
+    public static class Day01
+    {
+        public static Int64 Day01a(string[] input)
+        {
+            var sum = 0;
+            foreach (string s in input)
+            {
+                if (s.Length != 0)
+                {
+                    var digits = s.Where(c => Char.IsDigit(c));
+                    var first = digits.First() - '0';
+                    var last = digits.Last() - '0';
+                    sum += first * 10 + last;
+                }
+            }
+            return sum;
+        }
+
+        public static Int64 Day01b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day01.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day01a : {0}   Time: {1}", Day01a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day01b : {0}   Time: {1}", Day01b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day01/Day01Test.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC
+{
+    public class Day01Test
+    {
+        readonly string input =
+@"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+
+        readonly Int64 resultA = 142;
+        readonly Int64 resultB = 0;
+
+        [Fact]
+        public void Day01a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day01.Day01a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day01a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day01b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day01.Day01b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day01b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day01Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
