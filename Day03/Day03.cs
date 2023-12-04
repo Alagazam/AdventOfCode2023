c9f910a74365d206f3aa3e8ca3996c60260a2991
David Darj <david.darj@saabgroup.com>
2023-12-04 16:55:44 +0100

    Day 03 part 1

---
+++ Day03/Day03.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+namespace AoC
+{
+    public static class Day03
+    {
+        public static Int64 Day03a(string[] input)
+        {
+            var sum = 0;
+
+            for (int y = 0; y!=input.Length; ++y)
+            {
+                var number = 0;
+                var ispartnr = false;
+                for (int x = 0; x != input[y].Length; ++x)
+                {
+                    if (Char.IsDigit(input[y][x]))
+                    {
+                        number = number * 10 + input[y][x] - '0';
+                        if (!ispartnr)
+                        {
+                            ispartnr = CheckAdjacentCells(input, x, y);
+                        }
+                    }
+                    else
+                    {
+                        if (ispartnr)
+                        {
+                            sum += number;
+                        }
+                        number = 0;
+                        ispartnr= false;
+                    }
+                    if (x == input[y].Length-1 && ispartnr)
+                    {
+                        sum += number;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private static bool CheckAdjacent(string[] input, int x, int y)
+        {
+            try
+            {
+                if ("0123456789.".Contains(input[y][x])) return false;
+                return true;
+            }
+            catch (IndexOutOfRangeException)
+            { }
+
+            return false;
+        }
+
+        private static bool CheckAdjacentCells(string[] input, int x, int y)
+        {
+            return
+            CheckAdjacent(input, x - 1, y - 1) ||
+            CheckAdjacent(input, x    , y - 1) ||
+            CheckAdjacent(input, x + 1, y - 1) ||
+            CheckAdjacent(input, x - 1, y    ) ||
+            CheckAdjacent(input, x + 1, y    ) ||
+            CheckAdjacent(input, x - 1, y + 1) ||
+            CheckAdjacent(input, x    , y + 1) ||
+            CheckAdjacent(input, x + 1, y + 1);
+        }
+
+        public static Int64 Day03b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day03.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day03a : {0}   Time: {1}", Day03a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day03b : {0}   Time: {1}", Day03b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC
+{
+    public class Day03Test
+    {
+        readonly string input =
+@"467..114.
+...*.....
+..35..633
+......#..
+617*.....
+.....+.58
+..592....
+......755
+...$.*...
+.664.598.";
+
+        readonly Int64 resultA = 4361;
+        readonly Int64 resultB = 0;
+
+        [Fact]
+        public void Day03a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day03.Day03a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day03a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day03b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day03.Day03b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day03b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day03Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
