72eb35de2753947b96e939288c05cc1af7fec4b0
David Darj <zid@alagazam.net>
2023-12-10 21:31:27 +0100

    Day 10 part 2

--- Day10/Day10.cs
+++ Day10/Day10.cs
@@ -18,13 +18,16 @@ namespace AoC
             return new Point(-1, -1);
         }
 
-        public static int GetLength(string[] input, Point point, Point dir)
+        public static int GetLength(char[][] input, Point point, Point dir, bool markPath = false)
         {
             var length = 0;
             while (true)
             {
                 point = new Point(point.Item1 + dir.Item1, point.Item2 + dir.Item2);
+                if (point.Item1 < 0 || point.Item1 >= input.Length ||
+                    point.Item2 < 0 || point.Item2 >= input[point.Item1].Length) return -1;
                 var p = input[point.Item1][point.Item2];
+                if (markPath) input[point.Item1][point.Item2] = 'X';
                 ++length;
                 if (p == 'S') return length;
                 if (p == '.') return -1;
@@ -72,15 +75,16 @@ namespace AoC
 
         public static Int64 Day10a(string[] input)
         {
+            var in_copy = input.Select(s => s.ToArray()).ToArray();
             var start = GetStart(input);
             var length = 0;
-            length = GetLength(input, start, new Point(-1, 0));
+            length = GetLength(in_copy, start, new Point(-1, 0));
             if (length > 0) return length/2;
-            length = GetLength(input, start, new Point(1, 0));
+            length = GetLength(in_copy, start, new Point(1, 0));
             if (length > 0) return length/2;
-            length = GetLength(input, start, new Point(0, -1));
+            length = GetLength(in_copy, start, new Point(0, -1));
             if (length > 0) return length/2;
-            length = GetLength(input, start, new Point(0, 1));
+            length = GetLength(in_copy, start, new Point(0, 1));
             if (length > 0) return length/2;
 
             return 0;
@@ -88,7 +92,58 @@ namespace AoC
 
         public static Int64 Day10b(string[] input)
         {
-            return 0;
+            var start = GetStart(input);
+            var length = 0;
+            var in_copy = new char[input.Length][];
+            in_copy = input.Select(s => s.ToArray()).ToArray();
+
+            length = GetLength(in_copy, start, new Point(-1, 0), true);
+            if (length < 0)
+            {
+                in_copy = input.Select(s => s.ToArray()).ToArray();
+                length = GetLength(in_copy, start, new Point(1, 0), true);
+                if (length < 0)
+                {
+                    in_copy = input.Select(s => s.ToArray()).ToArray();
+                    length = GetLength(in_copy, start, new Point(0, -1), true);
+                    if (length < 0)
+                    {
+                        in_copy = input.Select(s => s.ToArray()).ToArray();
+                        length = GetLength(in_copy, start, new Point(0, 1), true);
+                        if (length < 0)
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            };
+            var incells = 0;
+            for (var r = 0; r != input.Length; r++)
+            {
+                bool inside = false;
+                char lastcorner = '.';
+                for (var c = 0; c != input[0].Length; c++)
+                {
+                    if (in_copy[r][c] == 'X')
+                    {
+                        if (input[r][c] == '|') inside = !inside;
+                        else if (input[r][c] == 'L' || input[r][c] == 'F') lastcorner = input[r][c];
+                        else if (input[r][c] == 'J' && lastcorner == 'F' ||
+                            input[r][c] == '7' && lastcorner == 'L')
+                        {
+                            inside = !inside;
+                            lastcorner = '.';
+                        }
+                    }
+                    else if (inside)
+                    {
+                        incells++;
+                        lastcorner = '.';
+                    }
+                }
+            }
+
+            return incells;
         }
 
 
--- Day10/Day10Test.cs
+++ Day10/Day10Test.cs
@@ -39,7 +39,6 @@ LJ.LJ
         readonly Int64 resultA2 = 4;
         readonly Int64 resultA3 = 8;
         readonly Int64 resultA4 = 8;
-        readonly Int64 resultB = 0;
 
         [Fact]
         public void Day10a1()
@@ -79,18 +78,85 @@ LJ.LJ
             Console.WriteLine("Day10a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
         }
 
+        readonly string input5 =
+@"...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+";
+        readonly Int64 resultB5 = 4;
 
         [Fact]
-        public void Day10b()
+        public void Day10b5()
         {
             var sw = Stopwatch.StartNew();
-            var lines = input1.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var lines = input5.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
 
             var result = Day10.Day10b(lines);
-            Assert.Equal(resultB, result);
+            Assert.Equal(resultB5, result);
 
             Console.WriteLine("Day10b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
         }
+
+        readonly string input6 =
+@"..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+";
+        readonly Int64 resultB6 = 4;
+
+
+        [Fact]
+        public void Day10b6()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input6.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day10.Day10b(lines);
+            Assert.Equal(resultB6, result);
+
+            Console.WriteLine("Day10b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        readonly string input7 =
+@"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+";
+        readonly Int64 resultB7 = 10;
+
+
+        [Fact]
+        public void Day10b7()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input7.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day10.Day10b(lines);
+            Assert.Equal(resultB7, result);
+
+            Console.WriteLine("Day10b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
 
         public Day10Test(ITestOutputHelper output)
         {
